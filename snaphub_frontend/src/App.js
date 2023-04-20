265e28d4fbac80c1ca133cb762f8218325746d00
Nimesh <nimesh.sakhiya2003@gmail.com>
2023-04-20 17:20:00 +0530

    login done

--- snaphub_frontend/src/App.js
+++ snaphub_frontend/src/App.js
@@ -1,11 +1,27 @@
-function App() {
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import Login from "./components/Login";
+import Home from "./container/Home";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const App = () => {
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+  //   if (!User) navigate('/login');
+  // }, []);
+
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <GoogleOAuthProvider clientId="86350847179-54ebd4nerqrs43j4gvtq4jhb342noa7n.apps.googleusercontent.com">
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </GoogleOAuthProvider>
   );
-}
+};
 
 export default App;
